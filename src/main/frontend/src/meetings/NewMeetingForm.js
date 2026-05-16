a76cd99baa2092663040ec281a11caf2eda4c3a4
rpole <rpolek85@gamil.com>
2026-05-16 15:12:39 +0200

    POST meetings

--- src/main/frontend/src/meetings/NewMeetingForm.js
+++ src/main/frontend/src/meetings/NewMeetingForm.js
@@ -9,6 +9,8 @@ export default function NewMeetingForm({onSubmit}) {
         onSubmit({title, description, participants: []});
     }
 
+
+
     return (
         <form onSubmit={submit}>
             <h3>Dodaj nowe spotkanie</h3>
